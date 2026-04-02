b11b1dbe522dcd7b95588a926f39662ecefdc547
julkostina <julikostina1504@gmail.com>
2026-04-02 13:57:17 +0300

    feat: update Vite configuration to support dynamic base path based on environment mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base:
+    process.env.VITE_BASE ??
+    (mode === "production" ? "./" : "/"),
+}));
